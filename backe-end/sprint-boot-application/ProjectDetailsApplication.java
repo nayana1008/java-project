6819ba6c091933a8141894a64f564115c7c28bf5
nayana1008 <71483034+nayana1008@users.noreply.github.com>
2020-10-02 09:01:45 +0530

    Add files via upload

---
+++ backe-end/sprint-boot-application/ProjectDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDetailsApplication.class, args);
+	}
+
+}
