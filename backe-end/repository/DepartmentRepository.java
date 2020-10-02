b630c7d1995ff8ecd686ced46f36d1aca5bc0f81
nayana1008 <71483034+nayana1008@users.noreply.github.com>
2020-10-02 09:07:50 +0530

    Add files via upload

--- backe-end/repository/DepartmentRepository.java
+++ backe-end/repository/DepartmentRepository.java
@@ -3,9 +3,9 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.model.Company;
+import com.example.demo.model.Department;
 
 @Repository
-public interface CompanyRepository extends JpaRepository<Company, Integer>{
+public interface DepartmentRepository extends JpaRepository<Department, Integer>{
 
 }
--- backe-end/repository/EmployeeRepository.java
+++ backe-end/repository/EmployeeRepository.java
@@ -3,9 +3,9 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.model.Company;
+import com.example.demo.model.Employee;
 
 @Repository
-public interface CompanyRepository extends JpaRepository<Company, Integer>{
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
 
 }
